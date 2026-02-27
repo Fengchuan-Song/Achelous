d483b8179f4be356fe2de7594a2fa9bbb5b2fb12
Fengchuan-Song <1319825810@qq.com>
2026-02-27 10:55:51 +0800

    modify a mistake

--- utils/utils_fit.py
+++ utils/utils_fit.py
@@ -404,7 +404,7 @@ def fit_one_epoch(model_train, model, ema, yolo_loss, loss_history, loss_history
             if (epoch + 1) % save_period == 0 or epoch + 1 == Epoch:
                 torch.save(save_state_dict, os.path.join(save_dir,
                                                          "ep%03d-loss%.3f-det_val_loss%.3f-seg_val_loss%.3f-seg_wl_val_loss%.3f.pth" % (
-                                                             epoch + 1, val_total_loss / epoch_step,
+                                                             epoch + 1, val_total_loss / epoch_step_val,
                                                              val_loss_det / epoch_step_val,
                                                              val_loss_seg / epoch_step_val,
                                                              val_loss_seg_w / epoch_step_val)))
